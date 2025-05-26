fcff71b81391903688e9ce6ad6e8ec2d0c848ddc
Trần Trung Hiếu <hieutran230504@gmail.com>
2025-05-26 22:05:32 +0700

    Add Folder

---
+++ Frontend/src/utils/router.js
@@ -0,0 +1,23 @@
+export const ROUTERS = {
+    USER: {
+        HOME: "",
+        NUOCHOA: "nuoc-hoa",
+        NUOCHOANAM: "nuoc-hoa-nam",
+        NUOCHOANU: "nuoc-hoa-nu",
+        NUOCHOAUNISEX: "nuoc-hoa-unisex",
+        FLASHSALE: "flash-sale",
+        THANHTOAN: "thanh-toan",
+        SIGNIN: "Sign-In",
+        SIGNUP: "Sign-Up",
+        CART: "Cart",
+        INSTRUCTION: "Instruction",
+        FORGOTPASS: "Forgot-Password",
+        RESETPASS: "Reset-Password",
+        PRODUCT_DETAIL: "Product-detail",
+        CONSULT: "consult",
+        ABOUT: "AboutUs",
+        BLOG: "Blog",
+        SEARCH: "Search",
+        PROFILE: "Profile"
+    }
+}
