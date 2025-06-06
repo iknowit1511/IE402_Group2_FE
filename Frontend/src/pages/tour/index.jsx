8b23e513184938dcba60d29d74cdbd8c7bfa9ba6
Trần Trung Hiếu <hieutran230504@gmail.com>
2025-06-06 22:12:22 +0700

    Add tour page

---
+++ Frontend/src/pages/tour/index.jsx
@@ -0,0 +1,67 @@
+import React, { memo, useState, useEffect } from "react";
+import { useSearchParams } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { CiSearch } from "react-icons/ci";
+import TourCard from "../../components/tourCard";
+import "./style.scss";
+
+function Tour() {
+  return (
+    <div className="tour-container">
+      <div className="breadcrumb">
+        <a href="/" className="breadcrumb-link">
+          Trang chủ
+        </a>
+        <span className="arrow"> &gt; </span>
+        <a href="/tour" className="breadcrumb-link">
+          <span className="current">Tour</span>
+        </a>
+      </div>
+
+      <div className="search_bar">
+        <input
+          type="text"
+          placeholder="Tìm kiếm tour bạn thích..."
+          id="searchText"
+          required
+        />
+        <button type="submit" className="search-button">
+          <i className="fa-solid fa-magnifying-glass">
+            <CiSearch strokeWidth={1} />
+          </i>
+        </button>
+      </div>
+
+      <div>
+        <h1 className="tour-title">Các tour phổ biến</h1>
+        <TourCard
+          price={2000000}
+          title="Huế Mộng Mơ – Dấu Ấn Cố Đô"
+          schedule="3 ngày 2 đêm"
+          rating={4.8}
+          numReviews={98}
+          imageUrls={[
+            "/images/image1.jpg",
+            "/images/image2.jpg",
+            "/images/image3.jpg",
+            "/images/image4.jpg",
+          ]}
+        />
+      </div>
+
+      <div>
+        <h1 className="tour-title">Các tour mới</h1>
+      </div>
+
+      <div>
+        <h1 className="tour-title">Các tour ngắn ngày</h1>
+      </div>
+
+      <div>
+        <h1 className="tour-title">Các tour dài ngày</h1>
+      </div>
+    </div>
+  );
+}
+
+export default memo(Tour);
