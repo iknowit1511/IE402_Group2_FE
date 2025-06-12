6267b00e07a8f716fc8b7991ff449704462ac644
Trần Trung Hiếu <hieutran230504@gmail.com>
2025-06-12 15:35:38 +0700

    Add getTourDetail service

--- Frontend/src/services/getTourDetail.js
+++ Frontend/src/services/getTourDetail.js
@@ -2,7 +2,7 @@ import api from "./axiosConfig";
 
 const getTourDetail = async (id) => {
     try {
-        const response = await api.get(`/perfumes/${id}`)
+        const response = await api.get(`/tours/${id}`)
         return response.data;
     } catch (error) {
         console.error('Error fetching products:', error);
